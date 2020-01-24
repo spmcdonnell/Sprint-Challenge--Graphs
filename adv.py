f294dc5bb050f8ed0e6b472020e0eb8f77c7b53a
Sean McDonnell <seanmcdonnell@Seans-MacBook-Pro.local>
2020-01-24 13:34:16 -0500

    MVP traversing map in about 1000 moves

--- adv.py
+++ adv.py
@@ -15,8 +15,8 @@ world = World()
 # map_file = "maps/test_line.txt"
 # map_file = "maps/test_cross.txt"
 # map_file = "maps/test_loop.txt"
-map_file = "maps/test_loop_fork.txt"
-# map_file = "maps/main_maze.txt"
+# map_file = "maps/test_loop_fork.txt"
+map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
 room_graph = literal_eval(open(map_file, "r").read())
@@ -29,8 +29,6 @@ player = Player(world.starting_room)
 
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n']
-traversal_path = []
-
 
 opposite_legend = {
     "n": "s",
@@ -40,27 +38,145 @@ opposite_legend = {
 }
 
 
+def get_opposite_direction(direction):
+    return opposite_legend[direction]
+
+
 def traverse_map():
+    full_path = []
     visited = {}
     prev_room = []
-    last_direction = ''
+    reverse_direction = ''
 
     while len(visited) < len(room_graph):
 
-        if player.current_room not in list(visited):
+        current_location = player.current_room
+        all_exits = player.current_room.get_exits()
 
+        if len(full_path) > 0:
+            reverse_direction = get_opposite_direction(full_path[-1])
+
+        ##############################
+        # Not visited yet
+        ##############################
+        if current_location.id not in list(visited):
+
+            ### Set unvisited room exit data ###
             room_data = {}
 
-            for direction in player.current_room.get_exits():
+            for direction in all_exits:
                 room_data[direction] = '?'
 
-            visited[player.current_room.id] = room_data
+            visited[current_location.id] = room_data
 
+            if len(prev_room):
+                visited[current_location.id][reverse_direction] = prev_room[-1]
+
+            ### Gather available potential moves from current location ###
+            possible_moves = []
+
+            for key in visited[current_location.id]:
+                possible_moves.append(visited[current_location.id][key])
+
+            ##############################
+            # If there are unknown paths
+            ##############################
+            if '?' in possible_moves:
+
+                ### List paths from current room that are unknown ###
+                unknown_paths = []
+
+                for key in visited[current_location.id]:
+                    if visited[current_location.id][key] == '?':
+                        unknown_paths.append(key)
+
+                ### Choose a random unknown path ###
+                random_unknown = random.choice(unknown_paths)
+
+                ### Keep track of last room before moving ###
+                prev_room.append(current_location.id)
+
+                ### Add to the path and then move there ###
+                full_path.append(random_unknown)
+                player.travel(random_unknown)
+
+                ### Update the direction from the previous room so that it points to the room just moved to instead of '?' ###
+                visited[current_location.id][random_unknown] = player.current_room.id
+
+            ##############################
+            # If all paths are known
+            ##############################
+            else:
+                prev_room_direction = ''
+
+                for key in visited[current_location.id]:
+                    if visited[current_location.id][key] == prev_room[-1]:
+                        prev_room_direction = key
+
+                ### Add to the path and then move there ###
+                full_path.append(prev_room_direction)
+                player.travel(prev_room_direction)
+
+                prev_room.pop()
+
+        ##############################
+        # Has been visited
+        ##############################
         else:
-            return
+            if visited[current_location.id][reverse_direction] == '?':
+                visited[current_location.id][reverse_direction] = prev_room[-1]
 
-    return
+            ### Gather available potential moves from current location ###
+            possible_moves = []
 
+            for key in visited[current_location.id]:
+                possible_moves.append(visited[current_location.id][key])
+
+            ##############################
+            # If there are unknown paths
+            ##############################
+            if '?' in possible_moves:
+
+                ### List paths from current room that are unknown ###
+                unknown_paths = []
+
+                for key in visited[current_location.id]:
+                    if visited[current_location.id][key] == '?':
+                        unknown_paths.append(key)
+
+                ### Choose a random unknown path ###
+                random_unknown = random.choice(unknown_paths)
+
+                ### Keep track of last room before moving ###
+                prev_room.append(current_location.id)
+
+                ### Add to the path and then move there ###
+                full_path.append(random_unknown)
+                player.travel(random_unknown)
+
+                ### Update the direction from the previous room so that it points to the room just moved to instead of '?' ###
+                visited[current_location.id][random_unknown] = player.current_room.id
+
+            ##############################
+            # If all paths are known
+            ##############################
+            else:
+                prev_room_direction = ''
+
+                for key in visited[current_location.id]:
+                    if visited[current_location.id][key] == prev_room[-1]:
+                        prev_room_direction = key
+
+                ### Add to the path and then move there ###
+                full_path.append(prev_room_direction)
+                player.travel(prev_room_direction)
+
+                prev_room.pop()
+
+    return full_path
+
+
+traversal_path = traverse_map()
 
 # TRAVERSAL TEST
 visited_rooms = set()
